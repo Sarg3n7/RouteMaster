0095106948f55a98c67c79d4f848726e3f6e9693
Sarg3n7 <shashwatshivam04@gmail.com>
2026-01-04 21:45:25 +0530

    load github api with optimization using loader and hook: useLoadData

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 const Github = () => {
-    const [data, setData] = useState([])
+  const data = useLoaderData()
+    // const [data, setData] = useState([])
 
-    useEffect(()=>{
-        fetch('https://api.github.com/users/Sarg3n7')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            setData(data)
-        })
-    },[])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/Sarg3n7')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // },[])
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl '>
         Github Followers: {data.followers}
@@ -22,7 +24,7 @@ const Github = () => {
 export default Github
 
 
-// export const githubInfoLoader = async () => {
-//   const response = await fetch('https://api.github.com/users/Sarg3n7')
-//   return response.json()
-// }
+export const githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/Sarg3n7')
+  return response.json()
+}
